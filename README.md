6e0f41581a2d25746600720bf5728fa2eeedb28c
Chris Waltrip <chris@walt.rip>
2023-08-06 18:34:29 -0700

    feat: looks like a fully working version.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # latex-glyph
 ![license](https://img.shields.io/github/license/cawaltrip/latex-glyph)
 
+[![made-with-latex](https://img.shields.io/badge/Made%20with-LaTeX-1f425f.svg)](https://www.latex-project.org/)
+
+
 $\LaTeX$ support for calling glyphs by their defined name.  Primarily intended (and tested) for working with icon fonts (e.g., Font Awesome, Nerd Fonts).  Currently requires $Lua\TeX$ with $\XeTeX$ support shortly thereafter.
+
---
+++ build.lua
@@ -0,0 +1,59 @@
+#!/usr/bin/env texlua
+
+-- Build script for glyph package
+
+bundle = ""
+module = "glyph"
+
+-- TDS zip file
+packtdszip = true
+
+-- Files to typeset
+typesetsourcefiles = {"glyph-lua.dtx"}
+typesetfiles = {"glyph.dtx"}
+-- typesetopts = "" -- get rid of noninteractive mode so we can get notified of errors.
+
+sourcefiles = {
+  "glyph.dtx",
+  "glyph.ins",
+  "glyph-messages.dtx",
+  "glyph-functions.dtx",
+  "glyph-functions-luatex.dtx",
+  "glyph-lua.dtx"
+}
+
+installfiles = {"*.sty", "glyph.lua"}
+
+-- ASCII mangling is not useful for us
+asciiengines = { }
+
+-- Avoid line-wrap issues
+maxprintline = 9999
+
+-- Need to typeset with LuaLaTeX since we're calling
+-- our own package in the documentation and it only
+-- supports LuaTeX currently.
+typesetexe = "lualatex"
+
+-- If we only want to document user manual, uncomment this line.
+-- typesetcmds = "\\AtBeginDocument{\\DisableImplementation}"
+
+uploadconfig = {
+    author      = "Chris Waltrip",
+    license     = "lppl1.3c",
+    summary     = "Arbitrary glyph command generation",
+    topic       = {"font-supp","font-supp-symbol"},
+    ctanPath    = "/macros/luatex/latex/glyph", -- or /support/glyph?
+    repository  = "https://github.com/cawaltrip/latex-glyph/",
+    bugtracker  = "https://github.com/cawaltrip/latex-glyph/issues",
+    update      = true,
+    description = [[
+  The glyph module provides document writers using LuaLaTeX a method of
+  using glyphs from any OTF font installed on the their system. This
+  methods uses the glyph's name (as defined inside the font) instead
+  of the Unicode codepoint.  Document-level commands are created for
+  each glyph in a predictable way, a fallback is provided. Lastly, a
+  command for showing every glyph (and its name) in a font is made
+  available.
+  ]]
+  }
